beffbf8d853ec1127047707f7f8fd74b89aef182
spycherf <spycher.frederic@gmail.com>
2020-03-09 13:39:31 +0100

    added logging of failed IDs

--- crawler.py
+++ crawler.py
@@ -5,11 +5,18 @@ import random
 import re
 import sys
 import time
-from datetime import datetime
 from bs4 import BeautifulSoup
+from datetime import datetime
 from urllib.error import HTTPError
 from urllib.error import URLError
 from urllib.request import urlopen
+
+
+def wait():
+    sleep_duration = random.randint(1, 60)
+    print("Too many requests. Sleeping for {0} seconds...".format(sleep_duration))
+    time.sleep(sleep_duration)
+    print("Trying again...")
 
 
 def http_request(url):
@@ -26,20 +33,14 @@ def http_request(url):
             # Handling "too many requests" errors
             if e.code == 429:
                 i += 1
-                sleep_duration = random.randint(1, 60)
-                print("Too many requests. Sleeping for {0} seconds...".format(sleep_duration))
-                time.sleep(sleep_duration)
-                print("Trying again...")
+                wait()
                 continue
             else:
                 raise
         # Handling "WinError 10054" errors
         except URLError:
             i += 1
-            sleep_duration = random.randint(1, 60)
-            print("Connection closed by host. Sleeping for {0} seconds...".format(sleep_duration))
-            time.sleep(sleep_duration)
-            print("Trying again...")
+            wait()
             continue
 
     src = response.read().decode("utf8", "ignore").replace("\r", " ").replace("\n", " ")
@@ -77,12 +78,12 @@ def get_data(url):
         match = re.search('(?<=q\(\"talkPage\.init\"\,)(\{.*\})', script.contents[0])
         full_JSON = json.loads(match.group(0))['__INITIAL_DATA__']
 
-    # Get ID
-    talk_id = full_JSON['current_talk']
+    # Get current talk ID
+    talk_id = full_JSON["current_talk"]
 
     # Extract JSON for the current talk
     current_talk_JSON = None
-    for talk in full_JSON['talks']:
+    for talk in full_JSON["talks"]:
         if not talk["id"] == talk_id:
             continue
         else:
@@ -97,17 +98,20 @@ def get_data(url):
     event = current_talk_JSON["event"]
     event_type = current_talk_JSON["video_type"]["name"]
     description = current_talk_JSON["description"]
-    tags = current_talk_JSON["tags"]
     duration = current_talk_JSON["duration"]
     nb_languages = len(current_talk_JSON["downloads"]["languages"])
     views = current_talk_JSON["viewed_count"]
+
+    tags = ""
+    for tag in current_talk_JSON["tags"]:
+        tags += (tag + ";")
 
     date_recorded = -1
     date_published = -1
     for player_talk in current_talk_JSON["player_talks"]:
         if player_talk["id"] == talk_id:
-            date_recorded = datetime.strptime(current_talk_JSON["recorded_at"][:10], "%Y-%m-%d")
-            date_published = datetime.fromtimestamp(player_talk["published"])
+            date_recorded = datetime.strptime(current_talk_JSON["recorded_at"][:10], "%Y-%m-%d").date()
+            date_published = datetime.fromtimestamp(player_talk["published"]).date()
             break
 
     nb_comments = -1
@@ -136,7 +140,9 @@ def get_data(url):
     }
 
 
-def crawl_and_update(output_file):
+def crawl_and_update(output_file,
+                     skip_file,
+                     log_file):
     csv_header = [
         "id", "url", "main_speaker", "title",
         "event", "event_type", "description", "tags",
@@ -147,17 +153,17 @@ def crawl_and_update(output_file):
     ]
 
     # Get list of IDs that have already been crawled
-    with open("to_skip.txt", "r") as f:
-        to_skip = [int(id) for id in f]
+    with open(skip_file, "r") as f:
+        to_skip = [int(talk_id) for talk_id in f]
 
-    # If crawling for the first time, writes header
+    # If crawling for the first time, write header
     if not os.path.isfile(output_file):
         with open(output_file, "w", newline="") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=csv_header)
             writer.writeheader()
 
     # Update records
-    with open(output_file, "a", newline="") as csvfile:
+    with open(output_file, "a", newline="", encoding="utf-8") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=csv_header)
         for i in range(1, 61000):
             sys.stdout.flush()
@@ -168,9 +174,13 @@ def crawl_and_update(output_file):
                 writer.writerow(get_data(url))
             except Exception as e:
                 print("Crawling ID {0} failed because of {1}".format(i, e))
+                with open(log_file, "a") as log:
+                    log.write(str(i) + "\n")
 
-            with open("to_skip.txt", "a") as f:
+            with open(skip_file, "a") as f:
                 f.write(str(i) + "\n")
 
 
-crawl_and_update("data/ted_talks_metadata_transcripts_FULL.csv")
+crawl_and_update("data/ted_talks_metadata_transcripts_FULL.csv",
+                 "to_skip.txt",
+                 "log.txt")
